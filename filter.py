c97ab7c5e82a8d8ecf884c4fde1f970a8f714384
Holden <164293854+BlackRasion@users.noreply.github.com>
2024-11-29 08:20:12 +0800

    optimization_01

--- filter.py
+++ filter.py
@@ -1,4 +1,5 @@
 from ipywidgets import widgets
+import cv2 as cv
 # 滤波器参数控件，默认隐藏
 filter_params = widgets.HBox([
     widgets.IntSlider(min=1, max=15, step=2, value=5, description='Kernel Size:', layout={'visibility': 'hidden'}),
@@ -7,3 +8,26 @@ filter_params = widgets.HBox([
     widgets.IntSlider(min=1, max=100, step=1, value=75, description='Sigma Color:', layout={'visibility': 'hidden'}),
     widgets.IntSlider(min=1, max=100, step=1, value=75, description='Sigma Space:', layout={'visibility': 'hidden'})
 ])
+
+# 均值滤波
+def apply_mean_filter(image, kernel_size):
+    kernel_size = kernel_size or 5
+    return cv.blur(image, (kernel_size, kernel_size))
+
+# 高斯滤波
+def apply_gaussian_filter(image, kernel_size, sigma_x):
+    kernel_size = kernel_size or 5
+    sigma_x = sigma_x or 1.5
+    return cv.GaussianBlur(image, (kernel_size, kernel_size), sigma_x)
+
+# 中值滤波
+def apply_median_filter(image, kernel_size):
+    kernel_size = kernel_size or 5
+    return cv.medianBlur(image, kernel_size)
+
+# 双边滤波
+def apply_bilateral_filter(image, diameter, sigma_color, sigma_space):
+    diameter = diameter or 9
+    sigma_color = sigma_color or 75
+    sigma_space = sigma_space or 75
+    return cv.bilateralFilter(image, diameter, sigma_color, sigma_space)
